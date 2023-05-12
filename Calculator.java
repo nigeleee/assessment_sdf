c62325c5d8fb41f8064f08068ce87b2dffec495c
nigeleee <nigeleee@gmail.com>
2023-05-12 16:51:13 +0800

    i really tried

--- Calculator.java
+++ Calculator.java
@@ -7,73 +7,57 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-
         String input = "";
-        
+
         System.out.println("Welcome.");
-        //Operator operators = new Operator();
-        
+        // Operator operators = new Operator();
+
         Scanner scanner = new Scanner(System.in);
 
         List<Integer> store = new ArrayList<>();
-        
-        while(!input.startsWith("exit")) {
-        
-        System.out.print("> ");
-        
-        int num1 = scanner.nextInt();
-       
-        String symbol = scanner.next();
 
-        int num2 = scanner.nextInt();
+        while (!input.equals("exit")) {
 
-        String cue = scanner.nextLine();
+            System.out.print("> ");
 
-        int result;
+            int num1 = scanner.nextInt();
 
+            String symbol = scanner.next();
 
-        if (input != null) {
-            switch (symbol) {
-                case "+":
-                result = num1 + num2;
-                store.add(result);
-                System.out.println(result);
-                break;
-
-                case "-":
-                result = num1 - num2;
-                store.add(result);
-                System.out.println(result);
-                break;
-
-                case "*":
-                result = num1 * num2;
-                store.add(result);
-                System.out.println(result);
-                break;
-
-                case "/":
-                result = num1 - num2;
-                store.add(result);
-                System.out.println(result);
-                break;
-            
-                } 
-
-            } 
-        
-        }
-      
-        System.out.println("Bye bye");
-            
-    }
-            
-}
-            
-            
-
-            
+            int num2 = scanner.nextInt();
   
+            int result;
+            
 
+            if (input != null) {
+                switch (symbol) {
+                    case "+":
+                        result = num1 + num2;
+                        store.add(result);
+                        System.out.println(result);
+                        break;
 
+                    case "-":
+                        result = num1 - num2;
+                        store.add(result);
+                        System.out.println(result);
+                        break;
 
+                    case "*":
+                        result = num1 * num2;
+                        store.add(result);
+                        System.out.println(result);
+                        break;
+
+                    case "/":
+                        result = num1 - num2;
+                        store.add(result);
+                        System.out.println(result);
+                        break;
+                }
+
+            }
+        } System.out.println("Bye bye");
+    }
+
+}
